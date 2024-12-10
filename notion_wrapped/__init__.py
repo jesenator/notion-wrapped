4c34ddff3599eb218e1b873cc2279f37b1d89ba9
Jesse Gilbert <jessegilbert6902@gmail.com>
2024-12-10 08:29:03 -0800

    new verision

--- notion_wrapped/__init__.py
+++ notion_wrapped/__init__.py
@@ -3,7 +3,7 @@ from .notion_client import NotionClient
 from .recurse import NotionRecurser
 from .notion_wrapped import main
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Jesse Gilbert"
 
 __all__ = [
